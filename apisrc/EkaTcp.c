dac8a2e04c6166d66d690f9c2339bcbdd0d5189f
Vitaly Sukonik <vitaly.sukonik@gmail.com>
2020-06-22 15:45:37 +0300

    Ignored RX pkt

--- apisrc/EkaTcp.c
+++ apisrc/EkaTcp.c
@@ -224,7 +224,8 @@ void ekaProcesTcpRx (EkaDev* dev, const uint8_t* pkt, uint32_t len) {
 	netIf->input(p,netIf);
       }
     } else {
-      EKA_WARN("Unexpected RX I/F: %s -- ignoring",EKA_MAC2STR(pkt));
+      hexDump("Ignored RX pkt",(uint8_t*)pkt,len);
+      //      EKA_WARN("Unexpected RX I/F: %s -- ignoring",EKA_MAC2STR(pkt));
       // Unexpected RX I/F -- Ignoring the pkt
     }
   }
