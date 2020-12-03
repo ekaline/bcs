49258528b1f164b250f9ea62c7a43cd0be33ff94
Vitaly Sukonik <vitaly.sukonik@gmail.com>
2020-12-03 13:10:05 +0200

    EkaHwVersion.c, ../new_api_tests/efhBoxPcapGap.c

---
+++ apisrc/EkaHwExpectedVersion.h
@@ -0,0 +1,8 @@
+#ifndef _EKA_HW_EXPECTED_VERSION_H_
+#define _EKA_HW_EXPECTED_VERSION_H_
+
+static const uint64_t EKA_EXPECTED_EPM_VERSION = 0x4;
+static const uint64_t EKA_EXPECTED_DMA_VERSION = 0x1;
+
+
+#endif
