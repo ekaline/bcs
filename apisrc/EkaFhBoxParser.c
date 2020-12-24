6cd690dce8d0e9145c26aab3786e1ad28e01846f
Vitaly Sukonik <vitaly.sukonik@gmail.com>
2020-12-24 14:31:53 +0200

    BOX EoM typo

--- apisrc/EkaFhBoxParser.c
+++ apisrc/EkaFhBoxParser.c
@@ -67,10 +67,10 @@ uint getHsvfMsgLen(const uint8_t* pkt, int bytes2run) {
   uint tableLen = hsvfTableLen(msg);
 
   if (tableLen != 0) {
-    if (pkt[tableLen - 1] != HsvfEom) {
+    if (pkt[tableLen] != HsvfEom) {
       hexDump("Hsvf Msg with wrong hsvfTableLen",pkt,bytes2run);
       on_error("tableLen of \'%c%c\' = %u, but last char = 0x%x",
-	       (char)msg[0],(char)msg[1],tableLen,pkt[tableLen - 1]);
+	       (char)msg[0],(char)msg[1],tableLen,pkt[tableLen]);
     }
     return tableLen;
   }
