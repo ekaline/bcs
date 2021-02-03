f26c0935bb149a3ce458a2999c44b7d640bd0eff
Vitaly Sukonik <vitaly.sukonik@gmail.com>
2021-02-03 16:45:12 +0200

    EfhPhlxTopoProps.h for testing

--- new_api_tests/EfhPhlxTopoProps.h
+++ new_api_tests/EfhPhlxTopoProps.h
@@ -3,43 +3,43 @@
 
 EkaProp efhPhlxTopoInitCtxEntries_A[] = {
 	{"efh.PHLX_TOPO.group.0.mcast.addr"   ,"233.47.179.104:18016"},
-	{"efh.PHLX_TOPO.group.0.snapshot.addr","206.200.151.82:18116"}, 	// TCP SOUPBIN
-	{"efh.PHLX_TOPO.group.0.recovery.addr","206.200.151.82:18116"}, 	// MOLD RECOVERY
+	{"efh.PHLX_TOPO.group.0.snapshot.addr","206.200.151.18:18116"}, 	// TCP SOUPBIN
+	{"efh.PHLX_TOPO.group.0.recovery.addr","206.200.151.18:18116"}, 	// MOLD RECOVERY
 	{"efh.PHLX_TOPO.group.0.snapshot.auth","STRK01:6EUG2W"},
 
 	{"efh.PHLX_TOPO.group.1.mcast.addr"   ,"233.47.179.105:18017"},
-	{"efh.PHLX_TOPO.group.1.snapshot.addr","206.200.151.83:18117"}, 	// TCP SOUPBIN
-	{"efh.PHLX_TOPO.group.1.recovery.addr","206.200.151.83:18117"}, 	// MOLD RECOVERY
+	{"efh.PHLX_TOPO.group.1.snapshot.addr","206.200.151.19:18117"}, 	// TCP SOUPBIN
+	{"efh.PHLX_TOPO.group.1.recovery.addr","206.200.151.19:18117"}, 	// MOLD RECOVERY
 	{"efh.PHLX_TOPO.group.1.snapshot.auth","STRK01:6EUG2W"},
 
 	{"efh.PHLX_TOPO.group.2.mcast.addr"   ,"233.47.179.106:18018"},
-	{"efh.PHLX_TOPO.group.2.snapshot.addr","206.200.151.84:18118"}, 	// TCP SOUPBIN
-	{"efh.PHLX_TOPO.group.2.recovery.addr","206.200.151.84:18118"}, 	// MOLD RECOVERY
+	{"efh.PHLX_TOPO.group.2.snapshot.addr","206.200.151.20:18118"}, 	// TCP SOUPBIN
+	{"efh.PHLX_TOPO.group.2.recovery.addr","206.200.151.20:18118"}, 	// MOLD RECOVERY
 	{"efh.PHLX_TOPO.group.2.snapshot.auth","STRK01:6EUG2W"},
 
 	{"efh.PHLX_TOPO.group.3.mcast.addr"   ,"233.47.179.107:18019"},
-	{"efh.PHLX_TOPO.group.3.snapshot.addr","206.200.151.85:18119"}, 	// TCP SOUPBIN
-	{"efh.PHLX_TOPO.group.3.recovery.addr","206.200.151.85:18119"}, 	// MOLD RECOVERY
+	{"efh.PHLX_TOPO.group.3.snapshot.addr","206.200.151.21:18119"}, 	// TCP SOUPBIN
+	{"efh.PHLX_TOPO.group.3.recovery.addr","206.200.151.21:18119"}, 	// MOLD RECOVERY
 	{"efh.PHLX_TOPO.group.3.snapshot.auth","STRK01:6EUG2W"},
 
 	{"efh.PHLX_TOPO.group.4.mcast.addr"   ,"233.47.179.108:18020"},
-	{"efh.PHLX_TOPO.group.4.snapshot.addr","206.200.151.86:18120"}, 	// TCP SOUPBIN
-	{"efh.PHLX_TOPO.group.4.recovery.addr","206.200.151.86:18120"}, 	// MOLD RECOVERY
+	{"efh.PHLX_TOPO.group.4.snapshot.addr","206.200.151.22:18120"}, 	// TCP SOUPBIN
+	{"efh.PHLX_TOPO.group.4.recovery.addr","206.200.151.22:18120"}, 	// MOLD RECOVERY
 	{"efh.PHLX_TOPO.group.4.snapshot.auth","STRK01:6EUG2W"},
 
 	{"efh.PHLX_TOPO.group.5.mcast.addr"   ,"233.47.179.109:18021"},
-	{"efh.PHLX_TOPO.group.5.snapshot.addr","206.200.151.87:18121"}, 	// TCP SOUPBIN
-	{"efh.PHLX_TOPO.group.5.recovery.addr","206.200.151.87:18121"}, 	// MOLD RECOVERY
+	{"efh.PHLX_TOPO.group.5.snapshot.addr","206.200.151.23:18121"}, 	// TCP SOUPBIN
+	{"efh.PHLX_TOPO.group.5.recovery.addr","206.200.151.23:18121"}, 	// MOLD RECOVERY
 	{"efh.PHLX_TOPO.group.5.snapshot.auth","STRK01:6EUG2W"},
 
 	{"efh.PHLX_TOPO.group.6.mcast.addr"   ,"233.47.179.110:18022"},
-	{"efh.PHLX_TOPO.group.6.snapshot.addr","206.200.151.88:18122"}, 	// TCP SOUPBIN
-	{"efh.PHLX_TOPO.group.6.recovery.addr","206.200.151.88:18122"}, 	// MOLD RECOVERY
+	{"efh.PHLX_TOPO.group.6.snapshot.addr","206.200.151.24:18122"}, 	// TCP SOUPBIN
+	{"efh.PHLX_TOPO.group.6.recovery.addr","206.200.151.24:18122"}, 	// MOLD RECOVERY
 	{"efh.PHLX_TOPO.group.6.snapshot.auth","STRK01:6EUG2W"},
 
 	{"efh.PHLX_TOPO.group.7.mcast.addr"   ,"233.47.179.111:18023"},
-	{"efh.PHLX_TOPO.group.7.snapshot.addr","206.200.151.89:18123"}, 	// TCP SOUPBIN
-	{"efh.PHLX_TOPO.group.7.recovery.addr","206.200.151.89:18123"}, 	// MOLD RECOVERY
+	{"efh.PHLX_TOPO.group.7.snapshot.addr","206.200.151.25:18123"}, 	// TCP SOUPBIN
+	{"efh.PHLX_TOPO.group.7.recovery.addr","206.200.151.25:18123"}, 	// MOLD RECOVERY
 	{"efh.PHLX_TOPO.group.7.snapshot.auth","STRK01:6EUG2W"},
 
 };
