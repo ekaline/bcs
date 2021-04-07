d27b86014d2c10aae3ce50657db2495c37b2a256
Vitaly Sukonik <vitaly.sukonik@gmail.com>
2021-04-07 14:43:11 +0300

    Makefiles cleanup for g++ versions

--- SmartNIC_SW/driver/ekaline_build_time.h
+++ SmartNIC_SW/driver/ekaline_build_time.h
@@ -1,2 +1,2 @@
-#define EKA_SN_DRIVER_BUILD_TIME "Tue Apr 6 15:22:18 IDT 2021"
+#define EKA_SN_DRIVER_BUILD_TIME "Wed Apr 7 14:41:02 IDT 2021"
 #define EKA_SN_DRIVER_RELEASE_NOTE "3_0_2_RC_WC6 ioctl fix for Centos 8 (4)"
