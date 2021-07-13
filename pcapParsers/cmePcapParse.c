6c91e9af3c024173323775dc10bb828ecf4c8f87
Vitaly Sukonik <vitaly.sukonik@gmail.com>
2021-07-13 18:54:23 +0300

    adding new CME group to cmePcapParse

--- pcapParsers/cmePcapParse.c
+++ pcapParsers/cmePcapParse.c
@@ -34,7 +34,7 @@ static GroupAddr group[] = {
     {inet_addr("224.0.32.2"), 15311, 0, 0},     // incrementFeed
     {inet_addr("224.0.32.1"), 15310, 0, 0},     // incrementFeed
 
-    {inet_addr("224.0.32.66"), 14342, 0, 0},    // Patrick
+    {inet_addr("224.0.31.66"), 14342, 0, 0},    // Patrick
 
 };
 
