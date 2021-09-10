b8d5e40e8b4268b540069aafa3fcb5bd4ac34107
Vitaly Sukonik <vitaly.sukonik@gmail.com>
2021-09-10 16:02:31 +0300

    Plr Recovery

--- apisrc/EkaFhPlr.c
+++ apisrc/EkaFhPlr.c
@@ -147,6 +147,12 @@ EkaOpResult EkaFhPlr::runGroups( EfhCtx* pEfhCtx, const EfhRunCtx* pEfhRunCtx, u
 
 	runGr->setGrAfterGap(gr->id);
 	gr->expected_sequence = gr->seq_after_snapshot;
+#ifdef PLR_CERT
+	uint64_t gapStart = gr->seq_after_snapshot - 10;
+	uint64_t gapEnd   = gapStart + 5;
+	EKA_LOG("#############\nPLR_CER artificial GAP closure: %ju..%ju\n#############",gapStart,gapEnd);
+	gr->closeIncrementalGap(pEfhCtx, pEfhRunCtx, gapStart,gapEnd);
+#endif 
       }
     }
       break;
@@ -162,12 +168,6 @@ EkaOpResult EkaFhPlr::runGroups( EfhCtx* pEfhCtx, const EfhRunCtx* pEfhRunCtx, u
 
 	runGr->setGrAfterGap(gr->id);
 	gr->expected_sequence = gr->seq_after_snapshot;
-#ifdef PLR_CERT
-	uint64_t gapStart = gr->seq_after_snapshot - 10;
-	uint64_t gapEnd   = gapStart + 5;
-	EKA_LOG("#############\nPLR_CER artificial GAP closure: %ju..%ju\n#############",gapStart,gapEnd);
-	gr->closeIncrementalGap(pEfhCtx, pEfhRunCtx, gapStart,gapEnd);
-#endif 
       }
     }
       break;
