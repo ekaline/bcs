1053ccc0d536fddca9ff04e01fef169f169e596a
Vitaly Sukonik <vitaly.sukonik@gmail.com>
2021-10-18 11:00:30 -0400

    EfhPlrProps.h for testing

--- new_api_tests/EfhPlrProps.h
+++ new_api_tests/EfhPlrProps.h
@@ -2,14 +2,15 @@
 #define _EFH_PLR_PROPS_H
 
 EkaProp efhArcaPlrInitCtxEntries_A[] = {
-	{"efh.ARCA_PLR.group.0.mcast.addr",      "224.0.60.9:41021"},
-	{"efh.ARCA_PLR.group.0.refresh.tcpAddr", "162.68.19.122:35050"},
-	{"efh.ARCA_PLR.group.0.refresh.udpAddr", "224.0.60.15:41027"},
-	{"efh.ARCA_PLR.group.0.retrans.tcpAddr", "162.68.19.122:35050"},
-	{"efh.ARCA_PLR.group.0.retrans.udpAddr", "224.0.60.12:41024"},
-	{"efh.ARCA_PLR.group.0.ChannelId","51"},
-	{"efh.ARCA_PLR.group.0.SourceId","ARGTSXD01"},
-
+    {"efh.ARCA_PLR.group.0.products","vanilla_book,vanilla_auction"},
+    {"efh.ARCA_PLR.group.0.mcast.addr",      "224.0.60.9:41021"},
+    {"efh.ARCA_PLR.group.0.refresh.tcpAddr", "162.68.19.122:35050"},
+    {"efh.ARCA_PLR.group.0.refresh.udpAddr", "224.0.60.15:41027"},
+    {"efh.ARCA_PLR.group.0.retrans.tcpAddr", "162.68.19.122:35050"},
+    {"efh.ARCA_PLR.group.0.retrans.udpAddr", "224.0.60.12:41024"},
+    {"efh.ARCA_PLR.group.0.ChannelId","51"},
+    {"efh.ARCA_PLR.group.0.SourceId","ARGTSXD01"},
+    
 };
 
 EkaProp efhArcaPlrInitCtxEntries_B[] = {
