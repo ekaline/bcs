f0c1aefd2c00aa55e7a2c60297b4611fbdbb347e
Vitaly Sukonik <vitaly.sukonik@gmail.com>
2022-01-20 17:39:55 +0200

    gap_test_compile.sh script fix

--- apisrc/gap_test_compile.sh
+++ apisrc/gap_test_compile.sh
@@ -8,5 +8,5 @@ else
     gap=$1
     delta=$2
     echo gap=\'$gap\' delta=\'$delta\'
-    make GCC_EXTERNAL_DEFINES="-D_EFH_TEST_GAP_INJECT_INTERVAL__=$gap -D_EFH_TEST_GAP_INJECT_DELTA_=$delta" -j 64
+    make GCC_EXTERNAL_DEFINES="-D_EFH_TEST_GAP_INJECT_INTERVAL_=$gap -D_EFH_TEST_GAP_INJECT_DELTA_=$delta" -j 64
 fi
