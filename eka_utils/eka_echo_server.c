63ea13251600b165f98929e814b86f0331adea4f
Vitaly Sukonik <vitaly.sukonik@gmail.com>
2022-03-24 16:02:19 +0200

    new_api_tests/efcCmeGts.c

--- eka_utils/eka_echo_server.c
+++ eka_utils/eka_echo_server.c
@@ -1,26 +1,3 @@
-/* echo-thread.c
- *
- * Copyright (c) 2000 Sean Walton and Macmillan Publishers.  Use may be in
- * whole or in part in accordance to the General Public License (GPL).
- *
- * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
- * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
- * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
- * SUCH DAMAGE.
-*/
-
-/*****************************************************************************/
-/*** echo-thread.c                                                         ***/
-/***                                                                       ***/
-/*** An echo server using threads.                                         ***/
-/*****************************************************************************/
 #include <stdlib.h>
 #include <errno.h>
 #include <unistd.h>
@@ -31,77 +8,82 @@
 #include <arpa/inet.h>
 #include <pthread.h>
 
-//#include "eka_hw_conf.h"
+#include "eka_macros.h"
 
-#define on_error(...) { fprintf(stderr, "FATAL ERROR: %s@%s:%d: ",__func__,__FILE__,__LINE__); fprintf(stderr, __VA_ARGS__); fprintf(stderr,"\n");perror(""); fflush(stdout); fflush(stderr); exit(1); }
+volatile bool keep_work = true;
 
-void PANIC(char* msg);
-#define PANIC(msg)  { perror(msg); exit(-1); }
+void  INThandler(int sig) {
+  signal(sig, SIG_IGN);
+  keep_work = false;
+  fprintf(stderr,"Ctrl-C detected: keep_work = false, exitting..."); fflush(stdout);fflush(stderr);
+  return;
+}
 
-/*--------------------------------------------------------------------*/
-/*--- Child - echo servlet                                         ---*/
-/*--------------------------------------------------------------------*/
-void* Child(void* arg)
-{   char line[100];
-    int bytes_read;
-    int client = *(int *)arg;
-
-    do
-    {
-        bytes_read = recv(client, line, sizeof(line), 0);
-        send(client, line, bytes_read, 0);
+void* Child(void* arg) {
+  int client = *(int *)arg;
+  
+  do {
+    char rxBuf[2000] = {};
+    ssize_t bytes_read = recv(client, rxBuf, sizeof(rxBuf), 0);
+    if (bytes_read > 0) {
+      hexDump("TCP RX",rxBuf,bytes_read);
+      send(client, rxBuf, bytes_read, 0);
     }
-    while (strncmp(line, "byebye\r", 7) != 0);
-    close(client);
-    return arg;
+  }  while (keep_work);
+  close(client);
+  
+  return arg;
 }
 
 /*--------------------------------------------------------------------*/
 /*--- main - setup server and await connections (no need to clean  ---*/
 /*--- up after terminated children.                                ---*/
 /*--------------------------------------------------------------------*/
-int main(void)
-{   int sd;
-    struct sockaddr_in addr;
+int main(int argc, char *argv[]) {
+  if (argc != 2)
+    on_error("TCP server port not specified: argc=%d",argc);
 
-    char hostname[1024];
-    gethostname(hostname, 1024);
-    if ((strcmp(hostname, "xn03") != 0) && (strcmp(hostname, "xn02") != 0) && (strcmp(hostname, "xn01") != 0)) {
-      on_error("Echo server must be run on ekaline lab xn01, xn02 or xn03 only");
-    } 
-    printf("Starting echo server on %s port %d\n",hostname,22222);
-    if ( (sd = socket(PF_INET, SOCK_STREAM, 0)) < 0 )
-        on_error("Socket");
-    int one_const = 1;
-    if (setsockopt(sd, SOL_SOCKET, SO_REUSEADDR, &one_const, sizeof(int)) < 0)
-      on_error("setsockopt(SO_REUSEADDR) failed");
+  signal(SIGINT, INThandler);
 
+  uint16_t port = atoi(argv[1]);
+    
+  printf("Starting echo server on port %d\n",port);
 
-    struct linger so_linger = {
-      true, // Linger ON
-      0     // Abort pending traffic on close()
-    };
-
-    if (setsockopt(sd,SOL_SOCKET,SO_LINGER,&so_linger,sizeof(struct linger)) < 0) on_error("Cant set SO_LINGER");
+  int sd = -1;
+  if ( (sd = socket(PF_INET, SOCK_STREAM, 0)) < 0 )
+    on_error("Socket");
+  int one_const = 1;
+  if (setsockopt(sd, SOL_SOCKET, SO_REUSEADDR, &one_const, sizeof(int)) < 0)
+    on_error("setsockopt(SO_REUSEADDR) failed");
 
 
-    addr.sin_family = AF_INET;
-    addr.sin_port = htons(22222);
-    addr.sin_addr.s_addr = INADDR_ANY;
-    if ( bind(sd, (struct sockaddr*)&addr, sizeof(addr)) != 0 )
-        on_error("Bind");
-    if ( listen(sd, 20) != 0 )
-        on_error("Listen");
-    while (1)
-    {   int client, addr_size = sizeof(addr);
-        pthread_t child;
+  struct linger so_linger = {
+			     true, // Linger ON
+			     0     // Abort pending traffic on close()
+  };
 
-        client = accept(sd, (struct sockaddr*)&addr,(socklen_t*) &addr_size);
-        printf("Connected from: %s:%d\n", inet_ntoa(addr.sin_addr), ntohs(addr.sin_port));
-        if ( pthread_create(&child, NULL, Child, &client) != 0 )
-            perror("Thread creation");
-        else
-            pthread_detach(child);  /* disassociate from parent */
-    }
-    return 0;
+  if (setsockopt(sd,SOL_SOCKET,SO_LINGER,&so_linger,sizeof(struct linger)) < 0)
+    on_error("Cant set SO_LINGER");
+
+  struct sockaddr_in addr = {};
+  addr.sin_family = AF_INET;
+  addr.sin_port = htons(port);
+  addr.sin_addr.s_addr = INADDR_ANY;
+  
+  if (bind(sd, (struct sockaddr*)&addr, sizeof(addr)) != 0)
+    on_error("Bind");
+  if (listen(sd, 20) != 0)
+    on_error("Listen");
+  while (keep_work) {
+    int client, addr_size = sizeof(addr);
+    pthread_t child;
+
+    client = accept(sd, (struct sockaddr*)&addr,(socklen_t*) &addr_size);
+    printf("Connected from: %s:%d\n", inet_ntoa(addr.sin_addr), ntohs(addr.sin_port));
+    if ( pthread_create(&child, NULL, Child, &client) != 0 )
+      perror("Thread creation");
+    else
+      pthread_detach(child);  /* disassociate from parent */
+  }
+  return 0;
 }
