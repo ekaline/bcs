e1de7749fd655f273e5c665b1fa16d7b36f228c5
Tomasz Wozniak <twozniak@striketechnologies.com>
2022-03-25 10:03:44 -0400

    Test: efcCmeTest - hardcode the gateway to work from nqxlxavt060p

--- new_api_tests/efcCmeTest.c
+++ new_api_tests/efcCmeTest.c
@@ -355,7 +355,7 @@ int main(int argc, char *argv[]) {
 	.attrs = {
 	    .host_ip      = inet_addr(clientIp.c_str()),
 	    .netmask      = inet_addr("255.255.255.0"),
-	    .gateway      = inet_addr("10.0.0.10"),
+	    .gateway      = inet_addr("10.120.10.1"), // from nqxlxavt060p
 	    .nexthop_mac  = {}, // resolved by our internal ARP
 	    .src_mac_addr = {}, // taken from system config
 	    .dont_garp    = 0
