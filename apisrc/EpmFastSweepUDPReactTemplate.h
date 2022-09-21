0ece62bd375d887411a845a5c21b4b62cb9cc5ad
evgeny <shumsky@ekaline.com>
2022-09-21 10:54:32 +0000

    usp cs template fix

--- apisrc/EpmFastSweepUDPReactTemplate.h
+++ apisrc/EpmFastSweepUDPReactTemplate.h
@@ -26,7 +26,7 @@ class EpmFastSweepUDPReactTemplate : public EpmTemplate {
 	{"udp_srcport"  , 2 , HwField::IMMEDIATE,    false, false},
 	{"udp_dstport"  , 2 , HwField::IMMEDIATE,    false, false},
 	{"udp_length"   , 2 , HwField::IMMEDIATE,    false, false},
-	{"udp_cs"       , 2 , HwField::IMMEDIATE,    false, false},
+	{"udp_cs"       , 2 , HwField::TCP_CHCK_SUM, false, true},
 	/* --------------------------- */
 	//42 bytes until now
 	/* --------------------------- */                     // swap, clear
