d152b6ae3e5cfe53962d91f5b04bbace8477a719
Vitaly Sukonik <vitaly.sukonik@gmail.com>
2022-11-03 10:08:43 +0200

    Glimpse properties update for Mrx2

--- apisrc/create_Mrx2Top_properties.pl
+++ apisrc/create_Mrx2Top_properties.pl
@@ -21,7 +21,8 @@ $mold_ip        = "206.200.182.";
 $mold_lsb_base  = 64;
 $mold_port      = 18100;
 
-$glimpse_ip     = "206.200.182.72";
+$glimpse_ip_base = "206.200.182.";
+$glimpse_ip_lsb  = 72;
 $glimpse_port   = 18300;
 
 $username = "MGGT04";
@@ -35,12 +36,15 @@ for ($i=0; $i<$groups;$i++) {
     print "\t\{\"efh.$exch_name.group.$i.products\",\"vanilla_book\"\},\n";
     print "\t\{\"efh.$exch_name.group.$i.mcast.addr\"   ,\"$mc_base$mc_lsb:$mc_port\"\},\n";
     print "\t\{\"efh.$exch_name.group.$i.recovery.addr\",\"$mold_ip$mold_lsb:$mold_port\"\}, \t// MOLD RECOVERY\n";
-    print "\t\{\"efh.$exch_name.group.$i.snapshot.addr\",\"$glimpse_ip:$glimpse_port\"\}, \t// TCP GLIMPSE\n";
+    print "\t\{\"efh.$exch_name.group.$i.snapshot.addr\",\"${glimpse_ip_base}${glimpse_ip_lsb}:$glimpse_port\"\}, \t// TCP GLIMPSE\n";
     print "\t\{\"efh.$exch_name.group.$i.snapshot.auth\",\"$username:$passwd\"\},\n";
     print "\n";
 
     $mc_port   ++;
     $mold_port ++;
+    $glimpse_port ++;
+    $glimpse_ip_lsb ++;
+    
 }
 print "};\n\n";
 #####################################################
@@ -52,8 +56,9 @@ $mold_ip        = "206.200.182.";
 $mold_lsb_base  = 192;
 $mold_port      = 18100;
 
-$glimpse_ip     = "206.200.182.72";
 $glimpse_port   = 18300;
+$glimpse_ip_base = "206.200.182.";
+$glimpse_ip_lsb  = 200;
 
 $username = "MGGT04";
 $passwd   = "UWYYFCYI";
@@ -66,12 +71,14 @@ for ($i=0; $i<$groups;$i++) {
     print "\t\{\"efh.$exch_name.group.$i.products\",\"vanilla_book\"\},\n";
     print "\t\{\"efh.$exch_name.group.$i.mcast.addr\"   ,\"$mc_base$mc_lsb:$mc_port\"\},\n";
     print "\t\{\"efh.$exch_name.group.$i.recovery.addr\",\"$mold_ip$mold_lsb:$mold_port\"\}, \t// MOLD RECOVERY\n";
-    print "\t\{\"efh.$exch_name.group.$i.snapshot.addr\",\"$glimpse_ip:$glimpse_port\"\}, \t// TCP GLIMPSE\n";
+    print "\t\{\"efh.$exch_name.group.$i.snapshot.addr\",\"${glimpse_ip_base}${glimpse_ip_lsb}:$glimpse_port\"\}, \t// TCP GLIMPSE\n";
     print "\t\{\"efh.$exch_name.group.$i.snapshot.auth\",\"$username:$passwd\"\},\n";
     print "\n";
 
     $mc_port       ++;
     $mold_port ++;
+    $glimpse_port ++;
+    $glimpse_ip_lsb ++;
 }
 print "};\n\n";
 #####################################################
