6f1f40781e8d482e873f9d6b3728471e240c41ca
Huynh Nguyen <huynhdev@live.com>
2023-02-14 12:02:45 -0500

    [GTSAVTAMER-12091] Re-add tail

--- apisrc/EpmBoeQuoteUpdateShortTemplate.h
+++ apisrc/EpmBoeQuoteUpdateShortTemplate.h
@@ -37,8 +37,9 @@ class EpmBoeQuoteUpdateShortTemplate : public EpmTemplate {
       {"MessageType" ,                  1, HwField::IMMEDIATE,    false, false }, // 0x59
       {"MatchingUnit" ,                 1, HwField::IMMEDIATE,    false, false }, // always 0
       {"SequenceNumber" ,               4, HwField::IMMEDIATE,    false, false }, // 0
-      {"QuoteUpdateIdtxt" ,             8, HwField::IMMEDIATE,    false, false }, // free text
+      {"QuoteUpdateIdtxt" ,             5, HwField::IMMEDIATE,    false, false }, // free text
       {"QuoteUpdateIdseq" ,             8, HwField::ASCII_CNT,    false, true  }, //
+      {"QuoteUpdateIdtail" ,            3, HwField::IMMEDIATE,    false, false }, // free text
       
       {"ClearingFirm" ,                 4, HwField::IMMEDIATE,    false, false },  
       {"ClearingAccount" ,              4, HwField::IMMEDIATE,    false, false },  
