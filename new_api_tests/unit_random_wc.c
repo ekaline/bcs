6d9eed1b6e311d310e86129af625f5c06e99278b
Vitaly Sukonik <vitaly.sukonik@gmail.com>
2023-02-15 10:48:08 +0200

    ../new_api_tests/unit_random_wc.c

--- new_api_tests/unit_random_wc.c
+++ new_api_tests/unit_random_wc.c
@@ -52,35 +52,38 @@ int main(int argc, char *argv[]) {
   uint8_t __attribute__ ((aligned(0x100))) data[RegionSize] = {};
   uint8_t __attribute__ ((aligned(0x100))) test_data[RegionSize] = {};
 
-  /* for (size_t i = 0; i < RegionSize; i ++) { */
-  /*   data[i] = 'a' + rand() % ('z' -'a' + 1); */
-  /* } */
+  for (auto j = 0; j < 1; j++) {
+    /* for (size_t i = 0; i < RegionSize; i ++) { */
+    /*   data[i] = 'a' + rand() % ('z' -'a' + 1); */
+    /* } */
 
-  for (size_t b = 0; b < RegionSize/BlockSize; b ++) {
-    for (size_t w = 0; w < BlockSize/sizeof(uint64_t); w ++) {
-      size_t flatWordIdx = b * BlockSize + w * sizeof(uint64_t);
-      sprintf((char*)&data[flatWordIdx],"b%2ju,w%ju;",b,w);
+    for (size_t b = 0; b < RegionSize/BlockSize; b ++) {
+      for (size_t w = 0; w < BlockSize/sizeof(uint64_t); w ++) {
+	size_t flatWordIdx = b * BlockSize + w * sizeof(uint64_t);
+	sprintf((char*)&data[flatWordIdx],"b%2ju,w%ju;",b,w);
+      }
     }
-  }
     
-  hexDump("Data",data,sizeof(data));
-  
-  copy_to_atomic(a2wr,data,sizeof(data)/8);
+    copy_to_atomic(a2wr,data,sizeof(data)/8);
 
-  uint64_t srcAddr = HwWcTestAddr / 8;
-  uint64_t* dstAddr = (uint64_t*)&test_data;
-  for (uint w = 0; w < roundUp64(sizeof(test_data)) / 8; w++)
-    SN_ReadUserLogicRegister(dev_id, srcAddr++, dstAddr++);
+    uint64_t srcAddr = HwWcTestAddr / 8;
+    uint64_t* dstAddr = (uint64_t*)&test_data;
+    for (uint w = 0; w < roundUp64(sizeof(test_data)) / 8; w++)
+      SN_ReadUserLogicRegister(dev_id, srcAddr++, dstAddr++);
 
-  hexDump("test_data",test_data,sizeof(data));
 
-  int r = memcmp(data,test_data,RegionSize);
-  if (r != 0) {
-    TEST_LOG("Failed: r = %d",r);
-  } else {
-    TEST_LOG("Succeeded");
+    int r = memcmp(data,test_data,RegionSize);
+    if (r != 0) {
+      TEST_LOG("Failed: r = %d",r);
+      hexDump("Data",data,sizeof(data));
+      hexDump("test_data",test_data,sizeof(data));
+      break;
+      
+    } else {
+      //      TEST_LOG("%d: Succeeded",j);
+    }
+    sleep(0);
   }
-  
   if (SC_CloseDevice(dev_id) != SC_ERR_SUCCESS) on_error("Error on SC_CloseDevice");
   return 0;
 }
