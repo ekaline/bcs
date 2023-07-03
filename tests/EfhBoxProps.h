3738c10609a598d2033d4219c111e86754c9f6ec
Vitaly Sukonik <vitaly@ekaline.com>
2023-07-03 13:46:45 +0300

    tests moved to libeka/tests

---
+++ tests/EfhBoxProps.h
@@ -0,0 +1,229 @@
+#ifndef _EFH_BOX_PROPS_H
+#define _EFH_BOX_PROPS_H
+
+EkaProp efhBoxInitCtxEntries_A[] = {
+	{"efh.BOX_HSVF.group.0.mcast.addr"   ,"224.0.124.1:21401"},
+	{"efh.BOX_HSVF.group.0.mcast.line"   ,"11"},
+	{"efh.BOX_HSVF.group.0.snapshot.addr","198.235.27.47:21410"}, 	// RECOVERY 
+	{"efh.BOX_HSVF.group.0.snapshot.auth","  :  "}, 			// RECOVERY 
+	{"efh.BOX_HSVF.group.0.recovery.addr","198.235.27.47:21410"}, 	// RECOVERY 
+
+	{"efh.BOX_HSVF.group.1.mcast.addr"   ,"224.0.124.2:22401"},
+	{"efh.BOX_HSVF.group.1.mcast.line"   ,"21"},
+	{"efh.BOX_HSVF.group.1.snapshot.addr","198.235.27.47:22410"}, 	// RECOVERY 
+	{"efh.BOX_HSVF.group.1.snapshot.auth","  :  "}, 			// RECOVERY 
+	{"efh.BOX_HSVF.group.1.recovery.addr","198.235.27.47:22410"}, 	// RECOVERY 
+
+	{"efh.BOX_HSVF.group.2.mcast.addr"   ,"224.0.124.3:23401"},
+	{"efh.BOX_HSVF.group.2.mcast.line"   ,"31"},
+	{"efh.BOX_HSVF.group.2.snapshot.addr","198.235.27.47:23410"}, 	// RECOVERY 
+	{"efh.BOX_HSVF.group.2.snapshot.auth","  :  "}, 			// RECOVERY 
+	{"efh.BOX_HSVF.group.2.recovery.addr","198.235.27.47:23410"}, 	// RECOVERY 
+
+	{"efh.BOX_HSVF.group.3.mcast.addr"   ,"224.0.124.4:24401"},
+	{"efh.BOX_HSVF.group.3.mcast.line"   ,"41"},
+	{"efh.BOX_HSVF.group.3.snapshot.addr","198.235.27.47:24410"}, 	// RECOVERY 
+	{"efh.BOX_HSVF.group.3.snapshot.auth","  :  "}, 			// RECOVERY 
+	{"efh.BOX_HSVF.group.3.recovery.addr","198.235.27.47:24410"}, 	// RECOVERY 
+
+	{"efh.BOX_HSVF.group.4.mcast.addr"   ,"224.0.124.5:25401"},
+	{"efh.BOX_HSVF.group.4.mcast.line"   ,"51"},
+	{"efh.BOX_HSVF.group.4.snapshot.addr","198.235.27.47:25410"}, 	// RECOVERY 
+	{"efh.BOX_HSVF.group.4.snapshot.auth","  :  "}, 			// RECOVERY 
+	{"efh.BOX_HSVF.group.4.recovery.addr","198.235.27.47:25410"}, 	// RECOVERY 
+
+	{"efh.BOX_HSVF.group.5.mcast.addr"   ,"224.0.124.6:26401"},
+	{"efh.BOX_HSVF.group.5.mcast.line"   ,"61"},
+	{"efh.BOX_HSVF.group.5.snapshot.addr","198.235.27.47:26410"}, 	// RECOVERY 
+	{"efh.BOX_HSVF.group.5.snapshot.auth","  :  "}, 			// RECOVERY 
+	{"efh.BOX_HSVF.group.5.recovery.addr","198.235.27.47:26410"}, 	// RECOVERY 
+
+	{"efh.BOX_HSVF.group.6.mcast.addr"   ,"224.0.124.7:27401"},
+	{"efh.BOX_HSVF.group.6.mcast.line"   ,"71"},
+	{"efh.BOX_HSVF.group.6.snapshot.addr","198.235.27.47:27410"}, 	// RECOVERY 
+	{"efh.BOX_HSVF.group.6.snapshot.auth","  :  "}, 			// RECOVERY 
+	{"efh.BOX_HSVF.group.6.recovery.addr","198.235.27.47:27410"}, 	// RECOVERY 
+
+	{"efh.BOX_HSVF.group.7.mcast.addr"   ,"224.0.124.8:28401"},
+	{"efh.BOX_HSVF.group.7.mcast.line"   ,"81"},
+	{"efh.BOX_HSVF.group.7.snapshot.addr","198.235.27.47:28410"}, 	// RECOVERY 
+	{"efh.BOX_HSVF.group.7.snapshot.auth","  :  "}, 			// RECOVERY 
+	{"efh.BOX_HSVF.group.7.recovery.addr","198.235.27.47:28410"}, 	// RECOVERY 
+
+};
+
+EkaProp efhBoxInitCtxEntries_B[] = {
+	{"efh.BOX_HSVF.group.0.mcast.addr"   ,"224.0.124.49:21404"},
+	{"efh.BOX_HSVF.group.0.mcast.line"   ,"11"},
+	{"efh.BOX_HSVF.group.0.snapshot.addr","198.235.27.55:21410"}, 	// RECOVERY 
+	{"efh.BOX_HSVF.group.0.snapshot.auth","                :                "}, 			// RECOVERY 
+	{"efh.BOX_HSVF.group.0.recovery.addr","198.235.27.55:21410"}, 	// RECOVERY 
+
+	{"efh.BOX_HSVF.group.1.mcast.addr"   ,"224.0.124.50:22404"},
+	{"efh.BOX_HSVF.group.1.mcast.line"   ,"21"},
+	{"efh.BOX_HSVF.group.1.snapshot.addr","198.235.27.55:22410"}, 	// RECOVERY 
+	{"efh.BOX_HSVF.group.1.snapshot.auth","                :                "}, 			// RECOVERY 
+	{"efh.BOX_HSVF.group.1.recovery.addr","198.235.27.55:22410"}, 	// RECOVERY 
+
+	{"efh.BOX_HSVF.group.2.mcast.addr"   ,"224.0.124.51:23404"},
+	{"efh.BOX_HSVF.group.2.mcast.line"   ,"31"},
+	{"efh.BOX_HSVF.group.2.snapshot.addr","198.235.27.55:23410"}, 	// RECOVERY 
+	{"efh.BOX_HSVF.group.2.snapshot.auth","                :                "}, 			// RECOVERY 
+	{"efh.BOX_HSVF.group.2.recovery.addr","198.235.27.55:23410"}, 	// RECOVERY 
+
+	{"efh.BOX_HSVF.group.3.mcast.addr"   ,"224.0.124.52:24404"},
+	{"efh.BOX_HSVF.group.3.mcast.line"   ,"41"},
+	{"efh.BOX_HSVF.group.3.snapshot.addr","198.235.27.55:24410"}, 	// RECOVERY 
+	{"efh.BOX_HSVF.group.3.snapshot.auth","                :                "}, 			// RECOVERY 
+	{"efh.BOX_HSVF.group.3.recovery.addr","198.235.27.55:24410"}, 	// RECOVERY 
+
+	{"efh.BOX_HSVF.group.4.mcast.addr"   ,"224.0.124.53:25404"},
+	{"efh.BOX_HSVF.group.4.mcast.line"   ,"51"},
+	{"efh.BOX_HSVF.group.4.snapshot.addr","198.235.27.55:25410"}, 	// RECOVERY 
+	{"efh.BOX_HSVF.group.4.snapshot.auth","                :                "}, 			// RECOVERY 
+	{"efh.BOX_HSVF.group.4.recovery.addr","198.235.27.55:25410"}, 	// RECOVERY 
+
+	{"efh.BOX_HSVF.group.5.mcast.addr"   ,"224.0.124.54:26404"},
+	{"efh.BOX_HSVF.group.5.mcast.line"   ,"61"},
+	{"efh.BOX_HSVF.group.5.snapshot.addr","198.235.27.55:26410"}, 	// RECOVERY 
+	{"efh.BOX_HSVF.group.5.snapshot.auth","                :                "}, 			// RECOVERY 
+	{"efh.BOX_HSVF.group.5.recovery.addr","198.235.27.55:26410"}, 	// RECOVERY 
+
+	{"efh.BOX_HSVF.group.6.mcast.addr"   ,"224.0.124.55:27404"},
+	{"efh.BOX_HSVF.group.6.mcast.line"   ,"71"},
+	{"efh.BOX_HSVF.group.6.snapshot.addr","198.235.27.55:27410"}, 	// RECOVERY 
+	{"efh.BOX_HSVF.group.6.snapshot.auth","                :                "}, 			// RECOVERY 
+	{"efh.BOX_HSVF.group.6.recovery.addr","198.235.27.55:27410"}, 	// RECOVERY 
+
+	{"efh.BOX_HSVF.group.7.mcast.addr"   ,"224.0.124.56:28404"},
+	{"efh.BOX_HSVF.group.7.mcast.line"   ,"81"},
+	{"efh.BOX_HSVF.group.7.snapshot.addr","198.235.27.55:28410"}, 	// RECOVERY 
+	{"efh.BOX_HSVF.group.7.snapshot.auth","                :                "}, 			// RECOVERY 
+	{"efh.BOX_HSVF.group.7.recovery.addr","198.235.27.55:28410"}, 	// RECOVERY 
+
+};
+
+const EkaGroup boxGroups[] = {
+	{EkaSource::kBOX_HSVF, (EkaLSI)0},
+	{EkaSource::kBOX_HSVF, (EkaLSI)1},
+	{EkaSource::kBOX_HSVF, (EkaLSI)2},
+	{EkaSource::kBOX_HSVF, (EkaLSI)3},
+	{EkaSource::kBOX_HSVF, (EkaLSI)4},
+	{EkaSource::kBOX_HSVF, (EkaLSI)5},
+	{EkaSource::kBOX_HSVF, (EkaLSI)6},
+	{EkaSource::kBOX_HSVF, (EkaLSI)7},
+};
+
+
+// RFQ (PIP A)
+EkaProp efhBoxInitCtxEntries_C[] = {
+	{"efh.BOX_HSVF.group.0.mcast.addr"   ,"224.0.124.17:21403"},
+	//	{"efh.BOX_HSVF.group.0.mcast.line"   ,"1P"},
+	{"efh.BOX_HSVF.group.0.mcast.line"   ,"11"},
+	{"efh.BOX_HSVF.group.0.snapshot.addr","198.235.27.47:21410"}, 	// RECOVERY 
+	{"efh.BOX_HSVF.group.0.snapshot.auth","  :  "}, 			// RECOVERY 
+	{"efh.BOX_HSVF.group.0.recovery.addr","198.235.27.47:21410"}, 	// RECOVERY 
+
+	{"efh.BOX_HSVF.group.1.mcast.addr"   ,"224.0.124.18:22403"},
+	//	{"efh.BOX_HSVF.group.1.mcast.line"   ,"2P"},
+	{"efh.BOX_HSVF.group.1.mcast.line"   ,"21"},
+	{"efh.BOX_HSVF.group.1.snapshot.addr","198.235.27.47:22410"}, 	// RECOVERY 
+	{"efh.BOX_HSVF.group.1.snapshot.auth","  :  "}, 			// RECOVERY 
+	{"efh.BOX_HSVF.group.1.recovery.addr","198.235.27.47:22410"}, 	// RECOVERY 
+
+	{"efh.BOX_HSVF.group.2.mcast.addr"   ,"224.0.124.19:23403"},
+	//	{"efh.BOX_HSVF.group.2.mcast.line"   ,"3P"},
+	{"efh.BOX_HSVF.group.2.mcast.line"   ,"31"},
+	{"efh.BOX_HSVF.group.2.snapshot.addr","198.235.27.47:23410"}, 	// RECOVERY 
+	{"efh.BOX_HSVF.group.2.snapshot.auth","  :  "}, 			// RECOVERY 
+	{"efh.BOX_HSVF.group.2.recovery.addr","198.235.27.47:23410"}, 	// RECOVERY 
+
+	{"efh.BOX_HSVF.group.3.mcast.addr"   ,"224.0.124.20:24403"},
+	//	{"efh.BOX_HSVF.group.3.mcast.line"   ,"4P"},
+	{"efh.BOX_HSVF.group.3.mcast.line"   ,"41"},
+	{"efh.BOX_HSVF.group.3.snapshot.addr","198.235.27.47:24410"}, 	// RECOVERY 
+	{"efh.BOX_HSVF.group.3.snapshot.auth","  :  "}, 			// RECOVERY 
+	{"efh.BOX_HSVF.group.3.recovery.addr","198.235.27.47:24410"}, 	// RECOVERY 
+
+	{"efh.BOX_HSVF.group.4.mcast.addr"   ,"224.0.124.21:25403"},
+	//	{"efh.BOX_HSVF.group.4.mcast.line"   ,"5P"},
+	{"efh.BOX_HSVF.group.4.mcast.line"   ,"51"},
+	{"efh.BOX_HSVF.group.4.snapshot.addr","198.235.27.47:25410"}, 	// RECOVERY 
+	{"efh.BOX_HSVF.group.4.snapshot.auth","  :  "}, 			// RECOVERY 
+	{"efh.BOX_HSVF.group.4.recovery.addr","198.235.27.47:25410"}, 	// RECOVERY 
+
+	{"efh.BOX_HSVF.group.5.mcast.addr"   ,"224.0.124.22:26403"},
+	//	{"efh.BOX_HSVF.group.5.mcast.line"   ,"6P"},
+	{"efh.BOX_HSVF.group.5.mcast.line"   ,"61"},
+	{"efh.BOX_HSVF.group.5.snapshot.addr","198.235.27.47:26410"}, 	// RECOVERY 
+	{"efh.BOX_HSVF.group.5.snapshot.auth","  :  "}, 			// RECOVERY 
+	{"efh.BOX_HSVF.group.5.recovery.addr","198.235.27.47:26410"}, 	// RECOVERY 
+
+	{"efh.BOX_HSVF.group.6.mcast.addr"   ,"224.0.124.23:27403"},
+	//	{"efh.BOX_HSVF.group.6.mcast.line"   ,"7P"},
+	{"efh.BOX_HSVF.group.6.mcast.line"   ,"71"},
+	{"efh.BOX_HSVF.group.6.snapshot.addr","198.235.27.47:27410"}, 	// RECOVERY 
+	{"efh.BOX_HSVF.group.6.snapshot.auth","  :  "}, 			// RECOVERY 
+	{"efh.BOX_HSVF.group.6.recovery.addr","198.235.27.47:27410"}, 	// RECOVERY 
+
+	{"efh.BOX_HSVF.group.7.mcast.addr"   ,"224.0.124.24:28403"},
+	//	{"efh.BOX_HSVF.group.7.mcast.line"   ,"8P"},
+	{"efh.BOX_HSVF.group.7.mcast.line"   ,"81"},
+	{"efh.BOX_HSVF.group.7.snapshot.addr","198.235.27.47:28410"}, 	// RECOVERY 
+	{"efh.BOX_HSVF.group.7.snapshot.auth","  :  "}, 			// RECOVERY 
+	{"efh.BOX_HSVF.group.7.recovery.addr","198.235.27.47:28410"}, 	// RECOVERY 
+
+};
+
+// Complex Orders (Strategy A)
+EkaProp efhBoxInitCtxEntries_D[] = {
+	{"efh.BOX_HSVF.group.0.mcast.addr"   ,"224.0.124.25:21407"},
+	{"efh.BOX_HSVF.group.0.mcast.line"   ,"1C"},
+	{"efh.BOX_HSVF.group.0.snapshot.addr","198.235.27.47:21410"}, 	// RECOVERY 
+	{"efh.BOX_HSVF.group.0.snapshot.auth","  :  "}, 			// RECOVERY 
+	{"efh.BOX_HSVF.group.0.recovery.addr","198.235.27.47:21410"}, 	// RECOVERY 
+
+	{"efh.BOX_HSVF.group.1.mcast.addr"   ,"224.0.124.26:22407"},
+	{"efh.BOX_HSVF.group.1.mcast.line"   ,"2C"},
+	{"efh.BOX_HSVF.group.1.snapshot.addr","198.235.27.47:22410"}, 	// RECOVERY 
+	{"efh.BOX_HSVF.group.1.snapshot.auth","  :  "}, 			// RECOVERY 
+	{"efh.BOX_HSVF.group.1.recovery.addr","198.235.27.47:22410"}, 	// RECOVERY 
+
+	{"efh.BOX_HSVF.group.2.mcast.addr"   ,"224.0.124.27:23407"},
+	{"efh.BOX_HSVF.group.2.mcast.line"   ,"3C"},
+	{"efh.BOX_HSVF.group.2.snapshot.addr","198.235.27.47:23410"}, 	// RECOVERY 
+	{"efh.BOX_HSVF.group.2.snapshot.auth","  :  "}, 			// RECOVERY 
+	{"efh.BOX_HSVF.group.2.recovery.addr","198.235.27.47:23410"}, 	// RECOVERY 
+
+	{"efh.BOX_HSVF.group.3.mcast.addr"   ,"224.0.124.28:24407"},
+	{"efh.BOX_HSVF.group.3.mcast.line"   ,"4C"},
+	{"efh.BOX_HSVF.group.3.snapshot.addr","198.235.27.47:24410"}, 	// RECOVERY 
+	{"efh.BOX_HSVF.group.3.snapshot.auth","  :  "}, 			// RECOVERY 
+	{"efh.BOX_HSVF.group.3.recovery.addr","198.235.27.47:24410"}, 	// RECOVERY 
+
+	{"efh.BOX_HSVF.group.4.mcast.addr"   ,"224.0.124.29:25407"},
+	{"efh.BOX_HSVF.group.4.mcast.line"   ,"5C"},
+	{"efh.BOX_HSVF.group.4.snapshot.addr","198.235.27.47:25410"}, 	// RECOVERY 
+	{"efh.BOX_HSVF.group.4.snapshot.auth","  :  "}, 			// RECOVERY 
+	{"efh.BOX_HSVF.group.4.recovery.addr","198.235.27.47:25410"}, 	// RECOVERY 
+
+	{"efh.BOX_HSVF.group.5.mcast.addr"   ,"224.0.124.30:26407"},
+	{"efh.BOX_HSVF.group.5.mcast.line"   ,"6C"},
+	{"efh.BOX_HSVF.group.5.snapshot.addr","198.235.27.47:26410"}, 	// RECOVERY 
+	{"efh.BOX_HSVF.group.5.snapshot.auth","  :  "}, 			// RECOVERY 
+	{"efh.BOX_HSVF.group.5.recovery.addr","198.235.27.47:26410"}, 	// RECOVERY 
+
+	{"efh.BOX_HSVF.group.6.mcast.addr"   ,"224.0.124.31:27407"},
+	{"efh.BOX_HSVF.group.6.mcast.line"   ,"7C"},
+	{"efh.BOX_HSVF.group.6.snapshot.addr","198.235.27.47:27410"}, 	// RECOVERY 
+	{"efh.BOX_HSVF.group.6.snapshot.auth","  :  "}, 			// RECOVERY 
+	{"efh.BOX_HSVF.group.6.recovery.addr","198.235.27.47:27410"}, 	// RECOVERY 
+
+	{"efh.BOX_HSVF.group.7.mcast.addr"   ,"224.0.124.32:28407"},
+	{"efh.BOX_HSVF.group.7.mcast.line"   ,"8C"},
+	{"efh.BOX_HSVF.group.7.snapshot.addr","198.235.27.47:28410"}, 	// RECOVERY 
+	{"efh.BOX_HSVF.group.7.snapshot.auth","  :  "}, 			// RECOVERY 
+	{"efh.BOX_HSVF.group.7.recovery.addr","198.235.27.47:28410"}, 	// RECOVERY 
+
+};
+#endif
