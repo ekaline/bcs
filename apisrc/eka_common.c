c57207557ab25f0d3452e50a2ba05e378850bbdc
Sol Toder <stoder@striketechnologies.com>
2023-07-25 10:20:36 -0400

    Clean up formatting

--- apisrc/eka_common.c
+++ apisrc/eka_common.c
@@ -59,19 +59,16 @@ void eka_get_time (char* t) {
   auto duration = now.time_since_epoch();
 
   typedef std::chrono::duration<int,
-    std::ratio_multiply<std::chrono::hours::period, std::ratio<8>
-    >::type> Days; /* UTC: +8:00 */
+    std::ratio_multiply<std::chrono::hours::period,
+    std::ratio<8>>::type> Days; /* UTC: +8:00 */
 
   Days days = std::chrono::duration_cast<Days>(duration);
   duration -= days;
-  auto hours = std::chrono::duration_cast<std::chrono::hours
-    >(duration);
+  auto hours = std::chrono::duration_cast<std::chrono::hours>(duration);
   duration -= hours;
-  auto minutes = std::chrono::duration_cast<std::chrono::minutes
-    >(duration);
+  auto minutes = std::chrono::duration_cast<std::chrono::minutes>(duration);
   duration -= minutes;
-  auto seconds = std::chrono::duration_cast<std::chrono::seconds
-    >(duration);
+  auto seconds = std::chrono::duration_cast<std::chrono::seconds>(duration);
   duration -= seconds;
   auto milliseconds = std::chrono::duration_cast<
     std::chrono::milliseconds>(duration);
