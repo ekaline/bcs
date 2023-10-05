5b38b9cdfc0b8a22c41fb09cd08f8905c441186c
Vitaly Sukonik <vitaly@ekaline.com>
2023-10-05 15:08:41 +0300

    deleted CMakeFiles

--- cereal/doc/CMakeFiles/update-doc.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/update-doc"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/update-doc.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
